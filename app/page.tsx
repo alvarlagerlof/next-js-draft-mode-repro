05bd7eb1fb23149e7bf75645c79d9584c6295c7c
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-07-02 01:20:10 +0200

    Add comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { draftMode } from "next/headers";
 import Link from "next/link";
 
+// Adding this line makes isEnabled always return false
+export const runtime = "edge"
+
 export default function Home() {
   const { isEnabled } = draftMode();
 
